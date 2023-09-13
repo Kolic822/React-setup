b706e4cf492cd242a563075dc40921ea35c01601
teo.kolic <teo.kolic@serengetitech.com>
2023-09-13 19:10:34 +0200

    Added state and buttons

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import { Cart } from "./pages/cart/cart";
 import { Shop } from "./pages/shop/shop";
+import { ShopContextProvider } from "./context/shop-context";
 
 const App = () => (
 	<div className="App">
-		<Router>
-			<Navbar />
-			<Routes>
-				<Route path="/" element={<Shop />}></Route>
-				<Route path="/cart" element={<Cart />}></Route>
-			</Routes>
-		</Router>
+		<ShopContextProvider>
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Shop />}></Route>
+					<Route path="/cart" element={<Cart />}></Route>
+				</Routes>
+			</Router>
+		</ShopContextProvider>
 	</div>
 );
 
--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
 
 export const Product = (props) => {
 	const { id, productName, price, productImage } = props.data;
+	const { addToCart, cartItems } = useContext(ShopContext);
+	const cartItemAmount = cartItems[id];
+
 	return (
 		<div className="product">
 			<img src={productImage} />
@@ -13,7 +17,9 @@ export const Product = (props) => {
 				<span>{price}</span>
 				<span> €</span>
 			</div>
-			<button>Add to cart</button>
+			<button className="addToCartBttn" onClick={() => addToCart(id)}>
+				Add to cart {cartItemAmount > 0 && <>({cartItemAmount})</>}
+			</button>
 		</div>
 	);
 };
