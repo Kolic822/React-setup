48df9d86aa5e99fbde8580eccdbfde99bb7d3a0a
teo.kolic <teo.kolic@serengetitech.com>
2023-09-15 09:23:29 +0200

    Added subtotal sum, removed warnings from console

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -10,6 +10,16 @@ const getDefaultCart = () => {
 export const ShopContextProvider = (props) => {
 	const [cartItems, setCartItems] = useState(getDefaultCart());
 
+	const getTotalCartAmount = () => {
+		return Object.keys(cartItems).reduce((totalAmount, item) => {
+			if (cartItems[item] > 0) {
+				const itemInfo = PRODUCTS.find((product) => product.id === Number(item));
+				return totalAmount + cartItems[item] * itemInfo.price;
+			}
+			return totalAmount;
+		}, 0);
+	};
+
 	const addToCart = (itemId) => {
 		setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
 	};
@@ -22,7 +32,7 @@ export const ShopContextProvider = (props) => {
 		setCartItems((prev) => ({ ...prev, [itemId]: newAmount }));
 	};
 
-	const contextValue = { cartItems, addToCart, removeFromCart, updateCartItemCount };
+	const contextValue = { cartItems, addToCart, removeFromCart, updateCartItemCount, getTotalCartAmount };
 
 	return <ShopContext.Provider value={contextValue}>{props.children}</ShopContext.Provider>;
 };
